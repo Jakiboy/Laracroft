8b58f28027485d35acd440e7b6d975750e282379
Jakiboy <j.sinnaour.official@gmail.com>
2026-08-18 04:39:46 +0100

    Create FilterableRequest.php

---
+++ src/Requests/FilterableRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Laracroft\Requests\Authentication;
+use Laracroft\Objects\FilterObject;
+
+abstract class FilterableRequest extends Authentication
+{
+    protected array $allowed = [];
+    protected array $allowedFilters = [];
+
+    protected const string DEFAULT_SORT_FIELD = 'id';
+
+    abstract protected function filterObjectClass() : string;
+
+    public function fromArray() : FilterObject
+    {
+        return $this->filterObjectClass()::fromArray(
+            $this->query(),
+            $this->allowed,
+            $this->allowedFilters,
+            static::DEFAULT_SORT_FIELD,
+        );
+    }
+
+    protected function prepareForValidation() : void
+    {
+        $this->merge([
+            'items' => $this->filled('items') ? (int)$this->input('items') : 10,
+            'page'  => $this->filled('page') ? (int)$this->input('page') : 1,
+        ]);
+    }
+
+    public function rules() : array
+    {
+        return [
+            'page'   => ['sometimes', 'integer', 'min:1'],
+            'items'  => ['sometimes', 'integer', 'min:1', 'max:500'],
+            'search' => ['sometimes', 'string', 'max:100'],
+            'sField' => ['sometimes', 'string', 'in:' . implode(',', $this->allowed)],
+            'sDir'   => ['sometimes', 'string', 'in:asc,desc'],
+        ];
+    }
+}
